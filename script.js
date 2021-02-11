f6650e46be01bd499ab1d5d2ca15fa424ea47f3c
emjose <emjose@gmail.com>
2021-02-11 09:30:37 -0500

    fix typo

---
+++ script.js
@@ -0,0 +1,17 @@
+// initial coffee value is zero
+
+let count = 0;
+
+// value and buttons
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function(e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease") {
+            count--;
+        })
+    })
+})
