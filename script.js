0733b9d47dfff553f5b03ea79cc5268f2fad6796
emmanueljose <emjose@gmail.com>
2022-09-22 18:18:17 -0400

    update structure and styling

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
-// initial coffee value is zero
+"use strict";
 
 let count = 0;
-
-// value and buttons
 
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const styles = e.currentTarget.classList;
-    if (styles.contains("decrease")) {
-      count--;
-    } else if (styles.contains("increase")) {
-      count++;
-    } else {
-      count = 0;
-    }
+	btn.addEventListener("click", function (e) {
+		const styles = e.currentTarget.classList;
+		if (styles.contains("decrease")) {
+			count--;
+		} else if (styles.contains("increase")) {
+			count++;
+		} else {
+			count = 0;
+		}
 
-    if (count > 0) {
-      value.style.color = "#fff";
-    }
-    if (count < 0) {
-      value.style.color = "#fff";
-    }
-    if (count === 0) {
-      value.style.color = "#fff";
-    }
-    value.textContent = count;
-  });
+		value.textContent = count;
+	});
 });
